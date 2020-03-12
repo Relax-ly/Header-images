a20af3f31fd3e61d8f819781eb9d72afe657ad05
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-03-12 15:28:44 -0700

    schema in progress, pushing now so i can create a new branch

--- db/index.js
+++ db/index.js
@@ -2,49 +2,56 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let listSchema = new mongoose.Schema({
-  name: String,
-  images: Array
-})
-
-let picSchema = new mongoose.Schema({
-  id: Number,
-  name: String,
-  url: String,
-  description: String
+  _id: Number,
+  images: [{
+    url: String,
+    description: String}]
 })
 
 let Listing = mongoose.model('Listing', listSchema);
-let picSchema = mongoose.model('Pic', picSchema);
 
 
-let ListingEnt = () => {
-  let listingName = 'some random name';
-  for (let i = 0; i < 6; i++) {
-    let newpic = new Pic({
-      id: i,
-      name: listingName, // so we can select pictures with this name and put into array
-      url: 'url',
-      description: 'brief description'
-    })
-    newpic.save((err) => {
-      if (!!err) {
-        console.log(err)
-      }
-    })
-  }
 
-  let imagesArr // do some query to select all images for listingName
 
-  let newListing = new Listing({
-  name: listingName,
-  images: imagesArr
-  })
 
-  newListing.save((err) => {
-    if (!!err) {
-      console.log(err)
-    }
-  })
-}
+// let picSchema = new mongoose.Schema({
+//   id: Number,
+//   name: String,
+//   url: String,
+//   description: String
+// })
+
+// let picSchema = mongoose.model('Pic', picSchema);
+
+
+// let ListingEnt = () => {
+//   let listingName = 'some random name';
+//   for (let i = 0; i < 6; i++) {
+//     let newpic = new Pic({
+//       id: i,
+//       name: listingName, // so we can select pictures with this name and put into array
+//       url: 'url',
+//       description: 'brief description'
+//     })
+//     newpic.save((err) => {
+//       if (!!err) {
+//         console.log(err)
+//       }
+//     })
+//   }
+
+//   let imagesArr // do some query to select all images for listingName
+
+//   let newListing = new Listing({
+//   name: listingName,
+//   images: imagesArr
+//   })
+
+//   newListing.save((err) => {
+//     if (!!err) {
+//       console.log(err)
+//     }
+//   })
+// }
 
 module.exports = Listing;
