2027b16fa085e96161a0d9fec8a5744bbe8e04ee
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-03-12 23:25:49 -0700

    React/webpack/babel setup

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client/src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$|.jsx$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader',
+               options: {
+                 presets: ['@babel/preset-react', '@babel/preset-env']
+               }
+              }
+      }
+    ]
+  },
+}
