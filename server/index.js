849c5cf9f90ef3ad53c2909dcf9bc489c2ac227c
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-03-17 14:41:57 -0700

    added some comments

--- server/index.js
+++ server/index.js
@@ -4,19 +4,23 @@ const controller = require('./controller.js');
 const bodyParser = require('body-parser');
 const db = require('../db');
 
+
 const app = express()
 const port = 2555
 
-// request the listing
+
+// request for a listing
 app.get(`/header-img/listing`, (req, res) => {
   controller.get(req, res);
 });
+
 
 // on componentDidMount(), populate the database
 app.get(`/header-img/get100`, (req, res) => {
   db.generate100();
   res.send('database was seeded')
 });
+
 
 app.use('/header-img', express.static(__dirname + '/../client/dist'))
 
