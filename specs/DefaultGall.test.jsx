3519617ca402b4ad02f2ad43fe0a2f430f03de9b
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-03-18 09:16:28 -0700

    simple test for DefaultGall

--- specs/DefaultGall.test.jsx
+++ specs/DefaultGall.test.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { shallow } from 'enzyme';
 import DefaultGall from '../client/src/DefaultGall.jsx';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDom.render(<Header />, div);
+describe('DefaultGall', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDom.render(<DefaultGall />, div);
+  });
+
+  it('should have 5 childrent', () => {
+    const wrapper = shallow(<DefaultGall />);
+    const children = wrapper.children();
+    expect(children.length).toBe(5);
+  });
+
+  it('should contain img element', () => {
+    const wrapper = shallow(<DefaultGall />);
+    const contains = wrapper.contains('img');
+    expect(contains).toBe(true);
+  });
+
+  it('should have 5 img element', () => {
+    const wrapper = shallow(<DefaultGall />);
+    const img = wrapper.find('img');
+    expect(img.length).toBe(5);
+  });
 });
