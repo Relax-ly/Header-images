89e1680bef85851bad92d3462a154b42e3d70793
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-03-18 15:10:29 -0700

    mock API request

--- specs/App.test.jsx
+++ specs/App.test.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { shallow } from 'enzyme';
+import axios from 'axios';
 import Header from '../client/src/App';
+
+jest.mock('axios');
+
+beforeEach(() => {
+
+});
 
 describe('Header component', () => {
   it('renders without crashing', () => {
@@ -10,6 +17,9 @@ describe('Header component', () => {
   });
 
   it('when rendered should get a listing', () => {
-
+    const getSpy = jest.spyOn(axios, 'get');
+    const HeaderInstance = shallow(<Header />);
+    expect(getSpy).toBeCalled();
   });
+
 });
