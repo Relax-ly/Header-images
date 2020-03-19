0222932bab999dec53d4ab0fe93c1594984f72f6
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-03-19 13:14:16 -0700

    created a style file

--- client/src/DefaultGall.jsx
+++ client/src/DefaultGall.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Image, Wrapper } from './style/style-DefaultGall';
 
 class DefaultGall extends React.Component {
   constructor(props) {
@@ -11,13 +13,14 @@ class DefaultGall extends React.Component {
   render() {
     console.log(this.props.listing);
     return (
-      <div>
-        <img src={this.props.listing.images[0].url} alt="first img" />
+      <Wrapper>
+        {/* <img src={this.props.listing.images[0].url} alt="first img" />
         <img src={this.props.listing.images[1].url} alt="second img" />
         <img src={this.props.listing.images[2].url} alt="third img" />
         <img src={this.props.listing.images[3].url} alt="fourt img" />
-        <img src={this.props.listing.images[4].url} alt="fifth img" />
-      </div>
+        <img src={this.props.listing.images[4].url} alt="fifth img" /> */}
+        <Image src={this.props.listing.images[0].url} alt="first img" />
+      </Wrapper>
     );
   }
 }
---
+++ client/src/style/Style-DefaultGall.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Image = styled.img`
+
+`;
+
+export const Wrapper = styled.div`
+`;
